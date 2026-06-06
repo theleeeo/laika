17c36147d7062a8e9db7da9fead3ca8228efe050
Leonard Forsberg <leonard.e.forsberg@gmail.com>
2026-06-06 14:27:02 +0200

    store types in the core in preparation for store breakout

---
+++ core/store.go
@@ -0,0 +1,24 @@
+package core
+
+import (
+	"context"
+
+	"github.com/theleeeo/indexer/model"
+)
+
+type Relation struct {
+	Parent model.Resource
+	Child  model.Resource
+}
+
+type Store interface {
+	AddChildResources(ctx context.Context, parent model.Resource, childs []model.Resource) error
+	AddRelations(ctx context.Context, relations []Relation) error
+	AnyResourceVersionDrifted(ctx context.Context, observed []model.VersionedResource) (bool, error)
+	DeleteResource(ctx context.Context, resource model.Resource) error
+	GetChildResources(ctx context.Context, parentResource model.Resource) ([]model.Resource, error)
+	GetParentResources(ctx context.Context, childResource model.Resource) ([]model.Resource, error)
+	NextRebuildCounter(ctx context.Context, resource model.Resource) (int64, error)
+	RemoveResource(ctx context.Context, resource model.Resource) error
+	UpsertResource(ctx context.Context, resource model.Resource, version int64) error
+}
